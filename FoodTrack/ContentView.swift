d07ecdd5e125397573e38c91b1ac9cdfb57cf55f
Morel <morelraphael2@gmail.com>
2025-03-27 22:06:35 +0100

    First commit

--- FoodTrack/ContentView.swift
+++ FoodTrack/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var food = ["Pates", "Salade", "Poulet"]
+    let caloriesPerGram: [String: Double] = [
+        "Pates": 12.5,
+        "Salade": 18.3,
+        "Poulet": 12.99
+    ]
+    @State var amount = 0.0
+    @State private var selectedFood = "Pâtes" // Valeur initiale
+    @State var totalCaloriesOfDay: Double = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section{
+                    HStack{
+                        TextField("Amount", value: $amount, format: .number.precision(.fractionLength(0)))
+                            .keyboardType(.decimalPad)
+                        Text("g")
+                    }
+                    Picker("Food", selection: $selectedFood){
+                        ForEach(food, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    Button("Add"){
+                        totalCaloriesOfDay+=(caloriesPerGram[selectedFood] ?? 0) * amount
+
+                    }
+
+                }
+                Section("Total Calories"){
+                    Text(totalCaloriesOfDay,format: .number.precision(.fractionLength(0)))
+                }
+            }
         }
-        .padding()
     }
 }
 
