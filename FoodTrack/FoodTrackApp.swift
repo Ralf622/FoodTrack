8882fe0d856453bb09e4e50b147961b16cac0752
Morel <morelraphael2@gmail.com>
2025-03-27 22:01:01 +0100

    Initial Commit

---
+++ FoodTrack/FoodTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTrackApp.swift
+//  FoodTrack
+//
+//  Created by Raphael Morel on 27/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
